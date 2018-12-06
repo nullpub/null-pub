dc535a778ec9e921ff722675fc25c6bdbb964df3
Brandon Blaylock <brandon@null.pub>
2018-12-05 23:58:17 -0800

    Extended error page, added fluid sizing

--- web/src/pages/ErrorPage/ErrorPage.tsx
+++ web/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import { history } from '../../libraries/history';
+
 import './ErrorPage.css';
 
 export interface ErrorPageProps {}
@@ -12,7 +14,9 @@ export interface ErrorPageProps {}
  */
 export const ErrorPage: React.SFC<ErrorPageProps> = () => (
   <article className="page-error">
-    <h1 className="ct-error">Error</h1>
+    <h1 className="ct-error ctr-error-hover ba-1 px-5 py-3 mtm-25p" onClick={() => history.push('/')}>
+      Error
+    </h1>
   </article>
 );
 
